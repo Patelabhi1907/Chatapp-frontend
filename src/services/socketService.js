95f22fc3c42c0a7f6bc45561b28de1aa7f64a0d5
Patelabhi1907 <gothiabhi2006@gmail.com>
2026-08-10 09:54:42 +0530

    port

--- src/services/socketService.js
+++ src/services/socketService.js
@@ -7,7 +7,7 @@ let socket = null;
 export function getSocket() {
     if (!socket) {
         socket = io(SOCKET_URL, {
-            transports: ['polling', 'websocket'],
+            transports: ['websocket', 'polling'],
             forceNew: false,
             reconnection: true,
         });
